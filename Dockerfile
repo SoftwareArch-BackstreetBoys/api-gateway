e339b0fd09a8f9b743276d40eb0c765e63b97041
Fei-7 <supachokang@gmail.com>
2024-10-16 22:04:07 +0700

    feat: initial custom-auth plugin

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,11 @@
 FROM kong:3.5.0
+
+USER root
+RUN apt update
+RUN apt install -y nodejs npm python3 make g++ vim
+RUN apt clean
+RUN npm i --unsafe -g kong-pdk@0.5.5
+
+COPY ./js-plugins/js-custom-auth /usr/local/kong/js-plugins/js-custom-auth
+WORKDIR /usr/local/kong/js-plugins/js-custom-auth
+RUN npm i
---
+++ js-plugins/js-custom-auth/index.js
@@ -0,0 +1,64 @@
+'use strict'
+
+const { version: Version } = require('./package.json')
+
+class CustomAuthPlugin {
+  constructor(config) {
+    this.config = config
+  }
+
+  async access(kong) {
+    const request_token_header_name = this.config.request_token_header
+    const user_id_header_name = this.config.user_id_header
+
+    const jwt_token = await kong.request.get_header(request_token_header_name)
+    if (!jwt_token) {
+      await kong.response.exit(401, { message: 'Access token required.' })
+      return
+    }
+
+    try {
+      const arrayToken = jwt_token.split('.')
+
+      const { expire_date, user_id } = JSON.parse(atob(arrayToken[1]))
+
+      if (!expire_date || !user_id) {
+        await kong.response.exit(401, { message: 'Invalid jwt token' })
+      }
+
+      if (new Date().getTime() >= expire_date*1000) {
+        await kong.response.exit(401, { message: 'Access token expired.' })
+        return
+      }
+
+      await kong.service.request.add_header(user_id_header_name, user_id);
+    } catch (error) {
+      const status = error.response && error.response.status ? error.response.status : 500
+      const body = error.response && error.response.data ? error.response.data : { message: 'Something went wrong. Please try again.' }
+      await kong.response.exit(status, body)
+    }
+  }
+}
+
+module.exports = {
+  Plugin: CustomAuthPlugin,
+  Name: 'js-custom-auth',
+  Schema: [
+    {
+      request_token_header: {
+        type: 'string',
+        required: true,
+        description: 'Header key name which hold jwt access token as a value'
+      }
+    },
+    {
+      user_id_header: {
+        type: 'string',
+        required: true,
+        description: 'Header key name you want the plugin to set user_id to'
+      }
+    },
+  ],
+  Version,
+  Priority: 0,
+}
