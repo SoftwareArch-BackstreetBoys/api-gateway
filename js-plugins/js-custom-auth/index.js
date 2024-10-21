5c36b8030631281c08ff80e8b148558f7e10615b
Fei-7 <supachokang@gmail.com>
2024-10-21 14:04:35 +0700

    feat: change custom auth plug in to get jwt token from cookie instead of header and enable user to add user id field to the body

--- js-plugins/js-custom-auth/index.js
+++ js-plugins/js-custom-auth/index.js
@@ -7,35 +7,63 @@ class CustomAuthPlugin {
     this.config = config
   }
 
-  async access(kong) {
-    const request_token_header_name = this.config.request_token_header
-    const user_id_header_name = this.config.user_id_header
+  getCookieValue(cookieString, cookieName) {
+    // Split the cookie string into individual cookies
+    const cookies = cookieString.split('; ');
 
-    const jwt_token = await kong.request.get_header(request_token_header_name)
+    // Loop through the cookies to find the one that matches the cookieName
+    for (let cookie of cookies) {
+      const [name, value] = cookie.split('=');
+      if (name === cookieName) {
+        return decodeURIComponent(value); // Return the cookie value
+      }
+    }
+
+    return null; // Return null if the cookie is not found
+  }
+
+  async getUserData(url, jwt_token) {
+    const response = await fetch(url, {
+      headers: {
+        'Cookie': `jwt=${jwt_token}; `  // Manually setting the cookie
+      }
+    })
+
+    if (!response.ok || response.status != 200) {
+      return null
+    }
+
+    return await response.json()
+  }
+
+  async access(kong) {
+    // const request_token_header_name = this.config.request_token_header
+    const user_id_field_name = this.config.user_id_field
+    const user_service_host = this.config.user_service_host
+
+    const cookie_header = await kong.request.get_header('Cookie')
+
+    const jwt_token = this.getCookieValue(cookie_header, 'jwt')
     if (!jwt_token) {
       await kong.response.exit(401, { message: 'Access token required.' })
       return
     }
 
     try {
-      const arrayToken = jwt_token.split('.')
+      const userData = this.getUserData(`${user_service_host}/auth/validate`, jwt_token)
+      
+      const userID = userData.id;
 
-      const { exp, Id } = JSON.parse(atob(arrayToken[1]))
+      await kong.service.request.add_header(user_id_field_name, userID);
+      if (this.config.add_to_body) {
+        const body = await kong.request.getBody();
 
-      if (!exp || !Id) {
-        await kong.response.exit(401, { message: 'Invalid jwt token' })
+        body[user_id_field_name] = userID
+
+        await kong.service.request.set_raw_body(body)
       }
-
-      if (new Date().getTime() >= exp*1000) {
-        await kong.response.exit(401, { message: 'Access token expired.' })
-        return
-      }
-
-      await kong.service.request.add_header(user_id_header_name, Id);
     } catch (error) {
-      const status = error.response && error.response.status ? error.response.status : 500
-      const body = error.response && error.response.data ? error.response.data : { message: 'Something went wrong. Please try again.' }
-      await kong.response.exit(status, body)
+      await kong.response.exit(500, { message: 'Something went wrong' })
     }
   }
 }
@@ -45,19 +73,31 @@ module.exports = {
   Name: 'js-custom-auth',
   Schema: [
     {
-      request_token_header: {
+      user_service_host: {
         type: 'string',
         required: true,
-        description: 'Header key name which hold jwt access token as a value'
+      }
+    },
+    // {
+    //   request_token_header: {
+    //     type: 'string',
+    //     required: true,
+    //     description: 'Header key name which hold jwt access token as a value'
+    //   }
+    // },
+    {
+      user_id_field: {
+        type: 'string',
+        required: true,
+        description: 'Header/Body key name you want the plugin to set user_id to'
       }
     },
     {
-      user_id_header: {
-        type: 'string',
-        required: true,
-        description: 'Header key name you want the plugin to set user_id to'
+      add_to_body: {
+        type: 'boolean',
+        default: false,
       }
-    },
+    }
   ],
   Version,
   Priority: 0,
